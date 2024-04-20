ccbdf77fd28dfd1aaa5d489272a6b33224a03d84
HHS <43839695+hhes0225@users.noreply.github.com>
2024-04-21 04:41:50 +0900

    Update AccountDB.cs

--- APIServer/HiveServer/Repository/AccountDB.cs
+++ APIServer/HiveServer/Repository/AccountDB.cs
@@ -11,16 +11,13 @@ namespace HiveServer.Repository;
 
 public class AccountDB:IAccountDB
 {
-	//SQL DB connection 시 필요 변수
 	private readonly IOptions<DBConfig> _dbConfig;
 	private IDbConnection _dbConnection;
 	private readonly SqlKata.Compilers.MySqlCompiler _compiler;
 	private readonly QueryFactory _queryFactory;
 	private readonly ILogger<AccountDB> _logger;
 
-    //생성자
-    //MySQL - HiveDB와 connect
-    //IOptions: ASP.NET Core에서 옵션을 구성하고 주입하기 위한 기능
+
     public AccountDB(ILogger<AccountDB> logger, IOptions<DBConfig> dbConfig)
 	{
 		_dbConfig = dbConfig;
@@ -32,11 +29,7 @@ public class AccountDB:IAccountDB
 		_queryFactory = new QueryFactory(_dbConnection, _compiler);
 
 	}
-
-	//Hive database의 Account 테이블과 연결하기 위한 클래스
-
-	
-
+ 
 	//HiveDB 서버와 Disconnect
 	public void Dispose()
 	{
@@ -84,7 +77,6 @@ public class AccountDB:IAccountDB
 
 
             //Insert문은 삽입 성공 시 삽입 성공한 데이터 개수를 반환함.
-            //1개의 데이터를 삽입하므로 계정 생성이 성공하면 항상 1이 반환될 것
             if (count != 1) {
                 return ErrorCode.CreateAccountDuplicatedUser;
             }
@@ -112,9 +104,7 @@ public class AccountDB:IAccountDB
                 return new Tuple<ErrorCode, String>(ErrorCode.LoginFailUserNotExist, "0");
             }
 
-            //비밀번호 비교
-            //들어온 비밀번호에 같은 로직으로 암호화했을 때, db 비밀번호와 같은지 확인
-            //같지 않으면 잘못된 비밀번호
+            //request 암호화 비밀번호 - DB 암호화 비밀번호 비교
             string hashedPasword = Security.HashPassword(accountInfo.salt_value, pw);
             if(hashedPasword != accountInfo.pw)
             {
@@ -142,9 +132,6 @@ public class AccountDB:IAccountDB
 	{
 		_dbConnection.Close();
 	}
-
-
-	//dbconn과 dbconfig 차이
 }
 
 public class DBConfig
