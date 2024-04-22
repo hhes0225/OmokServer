e261f92b841fbb2aed5449681ca69847ba622689
HHS <43839695+hhes0225@users.noreply.github.com>
2024-04-22 13:45:22 +0900

    Update AccountDB.cs

--- APIServer/HiveServer/Repository/AccountDB.cs
+++ APIServer/HiveServer/Repository/AccountDB.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Data;
-using SqlKata.Execution; //QueryFactory 사용하기 위함(그냥 SqlKata는 안됨)
+using SqlKata.Execution;
 using APIServer.Services;
 using Microsoft.Extensions.Options;
 using MySqlConnector;
@@ -11,39 +11,31 @@ namespace APIServer.Repository;
 
 public class AccountDB:IAccountDB
 {
-	//SQL DB connection 시 필요 변수
 	private readonly IOptions<DBConfig> _dbConfig;
 	private IDbConnection _dbConnection;
 	private readonly SqlKata.Compilers.MySqlCompiler _compiler;
 	private readonly QueryFactory _queryFactory;
 	private readonly ILogger<AccountDB> _logger;
 
-    //생성자
     //MySQL - HiveDB와 connect
-    //IOptions: ASP.NET Core에서 옵션을 구성하고 주입하기 위한 기능
     public AccountDB(ILogger<AccountDB> logger, IOptions<DBConfig> dbConfig)
 	{
 		_dbConfig = dbConfig;
 		_logger = logger;
 
-		Open();//DB connect private 메소드
+		Open();
 
 		_compiler = new SqlKata.Compilers.MySqlCompiler();
 		_queryFactory = new QueryFactory(_dbConnection, _compiler);
 
 	}
 
-	//Hive database의 Account 테이블과 연결하기 위한 클래스
-
-	
-
-	//HiveDB 서버와 Disconnect
 	public void Dispose()
 	{
 		Close();
 	}
 
-    //계정 생성 전, Hive DB에 해당 계정 존재 여부 조회
+    //Hive DB에 해당 계정 존재 여부 조회(계정 생성 요청 시 호출)
     public async Task<ErrorCode> FindAccountExistAsync(string email)
 	{  
         try
@@ -64,7 +56,7 @@ public class AccountDB:IAccountDB
         return ErrorCode.None;
     }
 
-    //계정 생성 시 DB에 사용자 추가
+    //DB에 사용자 추가(계정 생성 시 호출)
     public async Task<ErrorCode> CreateAccountAsync(string email, string pw)
 	{
         try
@@ -82,9 +74,6 @@ public class AccountDB:IAccountDB
                 recent_login_date = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")
             });
 
-
-            //Insert문은 삽입 성공 시 삽입 성공한 데이터 개수를 반환함.
-            //1개의 데이터를 삽입하므로 계정 생성이 성공하면 항상 1이 반환될 것
             if (count != 1) {
                 return ErrorCode.CreateAccountDuplicatedUser;
             }
@@ -95,10 +84,10 @@ public class AccountDB:IAccountDB
             return ErrorCode.CreateAccountFailException;
         }
 
-		return ErrorCode.None;//임의로 넣은 것
+		return ErrorCode.None;
 	}
 
-	//로그인: 사용자 인증
+	//사용자 인증(로그인 시 호출)
     public async Task<Tuple<ErrorCode, String>> VerifyUser(String email, string pw)
 	{
         string token;
@@ -114,14 +103,13 @@ public class AccountDB:IAccountDB
 
             //비밀번호 비교
             //들어온 비밀번호에 같은 로직으로 암호화했을 때, db 비밀번호와 같은지 확인
-            //같지 않으면 잘못된 비밀번호
             string hashedPasword = Security.HashPassword(accountInfo.salt_value, pw);
             if(hashedPasword != accountInfo.pw)
             {
                 return  new Tuple <ErrorCode, string> (ErrorCode.LoginFailWrongPassword, "0");
             }
 
-            //계정 정보가 맞다면 현재 시간으로 변경
+            //최근 로그인 시간 현재 시간으로 변경
             string newDate = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
             int count = _queryFactory.Query("Account").Where("email", email).Update(new {recent_login_date=newDate});
 
