8db41023896cc5b02ac8a71378d1925f7a5614e7
HHS <43839695+hhes0225@users.noreply.github.com>
2024-05-21 23:32:19 +0900

    Update Program.cs

--- SocketServer/SocketServer/Program.cs
+++ SocketServer/SocketServer/Program.cs
@@ -49,33 +49,7 @@ class Program
 
         await host.RunAsync();
     }
-    //static void Main(string[] args)
-    //{
-    //    //ParseCommandLine 메서드
-    //    var serverOption = ParseJsonFile("./appsettings.json");
-
-    //    //MainServer 클래스 변수 관련 설정
-    //    var serverApp = new MainServer();
-    //    serverApp.InitConfig(serverOption);
-    //    serverApp.CreateAndStartServer();
-
-    //    serverApp.MainLogger.Info("Press q to shutdown the server");
-
-    //    while (true)
-    //    {
-    //        if (Console.KeyAvailable)
-    //        {
-    //            ConsoleKeyInfo key = Console.ReadKey(true);
-
-    //            if (key.KeyChar == 'q')
-    //            {
-    //                Console.WriteLine("Server Terminated---");
-    //                serverApp.StopServer();
-    //                break;
-    //            }
-    //        }
-    //    }
-    //}
+    
 
     static ServerOption ParseCommandLine(string[] args)
     {
