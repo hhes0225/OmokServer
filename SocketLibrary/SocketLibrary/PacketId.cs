6f50156332caac7d1c4967991967cf127364c430
HHS <43839695+hhes0225@users.noreply.github.com>
2024-05-21 23:09:55 +0900

    Update PacketId.cs

--- SocketLibrary/SocketLibrary/PacketId.cs
+++ SocketLibrary/SocketLibrary/PacketId.cs
@@ -11,31 +11,6 @@ namespace SocketLibrary;
 //packet id = 요청 타입(패킷이 서버에게 어떤 기능을 해주기를 요청하는가)
 //1 ~ 10000번대 사용
 
-//1. mainServer.cs onConnected, onClosed, onPacketReceived로 이벤트(연결, 연결해제, 패킷받기) 감지
-//  *** 이때, 패킷 해석 결과 (ReceiveFilter.cs ResolveRequestInfo 메서드)는 onPacketReceived에서 호출된다
-//2. 위 이벤트 함수들에서 mainServer.cs Distribute()로 패킷 분석 결과를
-//  스레드(MainPacketProcessor-PacketProcessor.cs)에 넣는다(message buffer)
-//3. PacketProcess 클래스에는 여러 패킷 핸들러(PKHandler)가 있다. 패킷 헤더에서 PacketID를 뽑아와서
-//      PacketID 동작에 맞는 함수를 등록한다(PacketID: PacketDefine.cs)
-//      등록: PacketProcessor.cs->RegisterPacketHandler(여기서 PKHCommon, PKHRooms 패킷 핸들러 등록)
-//      (PacketHandleMap 딕셔너리에 등록 -> PKHCommon, PKHRooms에서 설정한 대로 자동으로 등록해줌)
-
-//      이때, 함수에는 함수 동작에 맞게 바디를 클래스화시켜주고 있다.
-
-//4. 등록한 함수는 언제 호출될까? MainServer에서 스레드를 돌릴 때
-//   PacketProcess.cs -> Process()에서 호출된다.
-//  이 부분은 2단계에서 버퍼에 넣은 패킷(주문서)를 읽어서 일꾼이 (주문서대로) 일을 처리하는 부분이다.
-//   var packet = MsgBuffer.Receive(); 에서 패킷을 리시브하고,
-
-//      if (PacketHandleMap.ContainsKey(packet.PacketID))
-//                {
-//                    PacketHandleMap[packet.PacketID] (packet);
-//                }
-//      이 패킷을 분석해서 딕셔너리에 맞는 패킷 핸들러 동작을 해준다.
-//      
-//      Receive가 되는 것은 버퍼에 값을 입력하는 Post가 들어올 때를 감지해서 Post가 들어오지 않으면 stop한다.
-//      Receive는 버퍼가 빈 버퍼가 될 때까지 수행한다.
-
 public enum PACKETID : int
 {
     ReqResTestEcho=101,
